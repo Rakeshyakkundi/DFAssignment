1d44747eff188febc0aa27cfa6171ca04d05747e
Rakesh yakkundi <rakeshyakkundi2k@gmail.com>
2022-07-02 22:53:16 +0530

    Add files via upload

---
+++ html_css_js/LWC  course/script.js
@@ -0,0 +1,9 @@
+//import minus,{PI,add} from './utils.js'
+//console.log(PI)
+//console.log(add(1,2))
+//console.log(minus(1,2))
+
+import * as Utils from './utils.js'
+console.log(Utils.PI)
+console.log(Utils.add(1,2))
+//console.log(minus(1,2)) // default not in all import 
---
+++ html_css_js/LWC  course/utils.js
@@ -0,0 +1,9 @@
+let PI=3.14
+
+function add(a,b){
+    return (a+b)
+}
+export {PI,add}
+export default function minus(a,b){
+    return (a-b)
+}
